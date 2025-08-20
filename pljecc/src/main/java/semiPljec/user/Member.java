4f000c71e364f6908c531286d493e950d2c813cb
dddd0ng <kdg1644@naver.com>
2025-08-20 12:25:22 +0900

    member

---
+++ pljecc/src/main/java/semiPljec/user/Member.java
@@ -0,0 +1,13 @@
+package semiPljec.user;
+
+public class Member {
+    private int memNo;
+    private String id;
+    private String pwd;
+    private String nickname;
+    
+    public Member(){
+
+    }
+
+}
