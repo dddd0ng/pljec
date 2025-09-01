a7999aa1d256abe04227e826b46c597b4b6dd8bc
dddd0ng <kdg1644@naver.com>
2025-09-01 17:30:51 +0900

    회원 탈퇴 시 탈퇴한 아이디,닉네임,이메일,전화번호 재사용 가능

--- pljecc/src/main/java/semiPljec/menu/RecommendMenu.java
+++ pljecc/src/main/java/semiPljec/menu/RecommendMenu.java
@@ -1,36 +1,36 @@
-package semiPljec.menu;
+    package semiPljec.menu;
 
-import java.io.Serializable;
+    import java.io.Serializable;
 
-//추천된 메뉴 1개를 표현하는 데이터 객체(DTO,엔티티 역할)
-//직렬화 가능, recommendedMenu.dat파일에 저장 가능
-public class RecommendMenu implements Serializable {
-    private int numNo;              //메뉴 추천 번호 (자동증가,menuRepository에서 관리)
-    private String menuName;        //추천된 메뉴 이름
+    //추천된 메뉴 1개를 표현하는 데이터 객체(DTO,엔티티 역할)
+    //직렬화 가능, recommendedMenu.dat파일에 저장 가능
+    public class RecommendMenu implements Serializable {
+        private int numNo;              //메뉴 추천 번호 (자동증가,menuRepository에서 관리)
+        private String menuName;        //추천된 메뉴 이름
 
-    public void setNumNo(int numNo) {
-        this.numNo = numNo;
+        public void setNumNo(int numNo) {
+            this.numNo = numNo;
+        }
+
+        public void setMenuName(String menuName) {
+            this.menuName = menuName;
+        }
+
+        public int getNumNo() {
+            return numNo;
+        }
+
+        public String getMenuName() {
+            return menuName;
+        }
+
+        public RecommendMenu(int numNo, String menuName) {
+            this.numNo = numNo;
+            this.menuName = menuName;
+        }
+
+        @Override
+        public String toString() {
+            return numNo + ". " + menuName;
+        }
     }
-
-    public void setMenuName(String menuName) {
-        this.menuName = menuName;
-    }
-
-    public int getNumNo() {
-        return numNo;
-    }
-
-    public String getMenuName() {
-        return menuName;
-    }
-
-    public RecommendMenu(int numNo, String menuName) {
-        this.numNo = numNo;
-        this.menuName = menuName;
-    }
-
-    @Override
-    public String toString() {
-        return numNo + ". " + menuName;
-    }
-}
